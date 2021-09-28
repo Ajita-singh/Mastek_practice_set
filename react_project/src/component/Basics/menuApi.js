b94fcb42632b1c1106569263322e5113a1fa9842
Ajita <34018203+Ajita-singh@users.noreply.github.com>
2021-09-28 15:55:04 +0530

    Add files via upload

---
+++ react_project/src/component/Basics/menuApi.js
@@ -0,0 +1,94 @@
+const Menu = [{
+    id: 1,
+    Image: "images/a.jfif",
+    name : "donuts",
+    category: "Breakfast",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing  dummy text ever since the 1500s,",
+
+},
+
+{
+    id: 2,
+    Image: "images/b.jfif",
+    name : "cupcake",
+    category: "Breakfast",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing  dummy text ever since the 1500s,",
+
+},
+
+{
+    id: 3,
+    Image: "images/c.jfif",
+    name : "chocoo",
+    category: "Evening",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing  dummy text ever since the 1500s,",
+
+},
+
+
+{
+    id: 4,
+    Image: "images/g.jfif",
+    name : "Pancakes",
+    category: "Dinner",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing dummy text ever since the 1500s,",
+
+},
+
+{
+    id: 5,
+    Image: "images/e.jfif",
+    name : "Oat Pancakes",
+    category: "lunch",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing dummy text ever since the 1500s,",
+
+},
+{
+    id: 6,
+    Image: "images/f.jfif",
+    name : "Omelette",
+    category: "lunch",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing dummy text ever since the 1500s,",
+
+},
+{
+    id: 7,
+    Image: "images/h.jfif",
+    name : "Ajita ",
+    category: "Spacial",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing dummy text ever since the 1500s,",
+
+},
+{
+    id: 8,
+    Image: "images/l.jfif",
+    name : "indiandishes",
+    category: "Breakfast",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing  dummy text ever since the 1500s,",
+
+},
+
+
+{
+    id: 9,
+    Image: "images/m.jfif",
+    name : "dhokla",
+    category: "Breakfast",
+    price: 25,
+    description : "    Lorem Ipsum is simply dummy text of the printing  dummy text ever since the 1500s,",
+
+},
+
+
+
+
+];
+export default Menu;
